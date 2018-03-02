dcb74c73c2ede6ad55fe668b9a351fc9eb6aac8a
LanceLRQ <lancelrq@gmail.com>
2018-03-02 11:03:05 +0800

    对于非Linux平台无法编译的兼容性处理

--- lorun/__init__.py
+++ lorun/__init__.py
@@ -1,1 +1,17 @@
-from ._lorun_ext import run, run_interactive, run_checker, check
+import platform
+import importlib
+
+def unsupport(*args, **kwargs):
+    print("Sorry, Lo-runner can only run in Linux.")
+
+if 'linux' in platform.system().lower():
+    try:
+        lorun = importlib.import_module('lorun')
+        run = lorun.run
+        run_interactive = lorun.run_interactive
+        run_checker = lorun.run_checker
+        check = lorun.check
+    except Exception as ex:
+        from ._lorun_ext import run, run_interactive, run_checker, check
+else:
+    run = run_interactive = run_checker = check = unsupport
