f308d1395d754f5cc7706455ebc2559870632592
LanceLRQ <lancelrq@gmail.com>
2018-10-02 11:18:20 +0800

    Fix 导包问题

--- lorun/__init__.py
+++ lorun/__init__.py
@@ -1,17 +1,22 @@
 import platform
-import importlib
+
 
 def unsupport(*args, **kwargs):
     print("Sorry, Lo-runner can only run in Linux.")
 
+
+def not_install(*args, **kwargs):
+    print("Sorry, Lo-runner not installed.")
+
+
 if 'linux' in platform.system().lower():
     try:
-        lorun = importlib.import_module('lorun')
-        run = lorun.run
-        run_interactive = lorun.run_interactive
-        run_checker = lorun.run_checker
-        check = lorun.check
-    except Exception as ex:
-        from lorun._lorun_ext import run, run_interactive, run_checker, check
+        try:
+            from lorun._lorun_ext import run, run_interactive, run_checker, check
+        except Exception as ex:
+            from ._lorun_ext import run, run_interactive, run_checker, check
+    except:
+        run = run_interactive = run_checker = check = not_install
+
 else:
     run = run_interactive = run_checker = check = unsupport
